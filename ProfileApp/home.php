136fe41699756b220508bf36867e8b0102aa044e
Pedro <pmecmail@gmail.com>
2023-10-17 14:33:38 +0200

    added nav folder with new navigation

--- ProfileApp/home.php
+++ ProfileApp/home.php
@@ -2,16 +2,13 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <header class="header">
-        <img src="./img/logo-no-background.png" alt="Logo">
-        <center><a href="home.html"><button>Home</button></a>
-            <a href="aboutMe.html"><button>About Me</button></a>
-            <a href="experience.html"><button>My Experience</button></a>
-            <a href="work.html"><button>My Work</button></a>
-            <a href="contact.html"><button>Contact Me</button></a>
+        <?php require 'navigation/nav.php'
+        ?>
     </header>
     </div>
     </header>
---
+++ ProfileApp/navigation/nav.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>About Me</title>
+<header class="header">
+  <div id="header">
+    <nav>
+      <img src="./img/logo-no-background.png" class="header-logo" alt="Logo">
+      <center><a href="index.php"><button>Home</button></a>
+        <a href="aboutMe.php"><button>About Me</button></a>
+        <a href="experience.php"><button>My Experience</button></a>
+        <a href="work.php"><button>My Work</button></a>
+        <a href="contact.php"><button>Contact Me</button></a>
+      </center>
+    </nav>
+  </div>
+</header>
