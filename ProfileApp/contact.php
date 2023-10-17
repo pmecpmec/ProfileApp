df955576602c3bc347be92aa2e24566a66daaba4
Pedro <pmecmail@gmail.com>
2023-10-17 12:31:01 +0200

    made everything in php

---
+++ ProfileApp/contact.php
@@ -0,0 +1,32 @@
+<!-- //Developer: Emma -->
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ <header>
+  <header class="header">
+    <img src="./img/logo-no-background.png" alt="Logo">
+      <center><a href="index.html"><button>Home</button></a>
+        <a href="aboutMe.html"><button>About Me</button></a>
+        <a href="experience.html"><button>My Experience</button></a>
+       <a href="work.html"><button>My Work</button></a>
+       <a href="contact.html"><button>Contact Me</button></a></header></center>
+  </header>
+ </header>
+ <br/> <br/>
+
+ <div><img src="img/headerLogo.png" alt="Logo"></div>
+   <a href="https://www.instagram.com/" target="_blank">
+   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png" alt="Instagram" style="width:50px;height:50px;">
+      <a href="https://www.facebook.com/" target="_blank">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/2048px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook" style="width:50px;height:50px;">
+          <a href="https://www.linkdin.com/" target="_blank">
+          <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="linkdin" style="width:50px;height:50px;">
+            <a href="https://www.twitter.com/" target="_blank">
+            <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-twitter-social-media-round-icon-png-image_6315985.png" alt="Twitter" style="width:50px;height:50px;">
+  </a>
+</body>
+</html>
