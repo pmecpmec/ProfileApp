8154ae4f4b84b79f7f4fdbab7abf28cba422bb5a
Pedro <pmecmail@gmail.com>
2023-10-17 13:12:07 +0200

    fixed a bit of home.php

---
+++ ProfileApp/nav/nav.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About Me</title>
+</head>
+
+<body>
+  <header class="header">
+    <nav>
+      <div id="header">
+        <img src="./img/logo-no-background.png" alt="Logo">
+        <center>
+          <a href="index.php"><button>Home</button></a>
+          <a href="aboutMe.php"><button>About Me</button></a>
+          <a href="experience.php"><button>My Experience</button></a>
+          <a href="work.php"><button>My Work</button></a>
+          <a href="contact.php"><button>Contact Me</button></a>
+        </center>
+      </div>
+    </nav>
+  </header>
+</body>
+
+</html>
