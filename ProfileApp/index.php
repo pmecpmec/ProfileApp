09cfc1a36b04747d4d9bc6b720e66fd14897851a
ebram101 <144430149+ebram101@users.noreply.github.com>
2023-10-17 16:25:05 +0200

    changed

--- ProfileApp/index.php
+++ ProfileApp/index.php
@@ -15,32 +15,23 @@
 
 <body>
   <br>
-  <h3>
-    <p>
-      <center>We zijn blij dat je er bent. Hier vind je allerlei informatie en hulpmiddelen om je te helpen bij je reis.
-    </p>
-    </center>
-  </h3>
   <form action="index.php" method="post">
-    <div class="register">
-      <h1>Register</h1>
-      <p> Please Fill in this form to create an account.</p>
- 
+    <div class="login">
+      <h1> Login to start your journey</h1>
+    
+      <label for="email"><b> email</label></b>
+      <input type="email" placeholder="Enter email" name="email"  id="email" required>
 
-<label for="email"><b> email</label></b>
-<input type="email" placeholder="Enter email" name="email"  id="email" required>
+      <label for="password"><b> password</label></b>
+      <input type="password" placeholder="Enter password" name="password"  id="password" required>
 
-<label for="password"><b> password</label></b>
-<input type="password" placeholder="Enter password" name="password"  id="password" required>
+      <button type="submit" class="loginbtn">log in</button>
 
-
-<button type="sumbit" class="registerbtn">Register</button>
-</div>
-<div class="container signin">
-  <p> already have an account <a href="index.php">sign in </a> </p>
-
-
-
+      <div class="container signin">
+        <p> not registered yet ? <a href="register.php">sign in </a> </p>
+      </div>
+    </div>
+  </form>
 </body>
 <footer>
 </footer>
