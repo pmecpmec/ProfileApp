6b4b21ab41b5b065ee5e688efc0fcc002a5b170c
Kim Nguyen <kim.nguyen@windesheim.nl>
2023-10-26 15:11:32 +0200

    Frontend almost done

--- ProfileApp/aboutMe.php
+++ ProfileApp/aboutMe.php
@@ -1,15 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <header class="header">
-    <?php require 'navigation/nav.php'
-    ?>
-  </header>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+    <header class="header">
+        <nav>
+            <div id="header">
+                <img src="./img/logo-no-background.png" alt="Logo">
+                <center><a href="index.html"><button>Home</button></a>
+                    <a href="aboutMe.html"><button>About Me</button></a>
+                    <a href="experience.html"><button>My Experience</button></a>
+                    <a href="work.html"><button>My Work</button></a>
+                    <a href="contact.html"><button>Contact Me</button></a></header></center>
+    </div>
+    </nav>
+    </header>
 </head>
-
 <body>
 
-</body>
+<div class="grid-container">
+    <div class="grid-item">1</div>
+    <div class="grid-item">
+        <div class="card">
+            <div class="container">
+                <div class="banner-container">
+                    <div class="banner"><img class="profilePicture" src="img/logo-color.png" alt="Avatar" style="width:200px"></div>
+                    <div class="banner">
+                        <div class="banner-grid-item"><h4>John Doe</h4></div>
+                        <div class="banner-grid-item"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="grid-item">3</div>
+    <div class="grid-item">4</div>
+    <div class="grid-item">
+        <div class="card">
+            <div class="container">
+                <b class="title">About me</b>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+    <div class="grid-item">6</div>
+    <div class="grid-item">4</div>
+    <div class="grid-item">
+        <div class="card">
+            <div class="container">
+                <b class="title">Schoolprestatie</b>
+                <p>Naam</p>
+                <p>Jaar</p>
+                <p>Plaats</p>
+            </div>
+        </div>
+    </div>
+    <div class="grid-item">6</div>
+    <div class="grid-item">4</div>
+    <div class="grid-item">
+        <div class="card">
+            <div class="container">
+                <b class="title">Werkervaring</b>
+                <p>Naam</p>
+                <p>Jaar</p>
+                <p>Plaats</p>
+            </div>
+        </div>
+    </div>
+    <div class="grid-item">6</div>
+    <div class="grid-item">4</div>
+    <div class="grid-item">
+        <div class="card">
+            <div class="container">
+                <b class="title">Hobby's</b>
+                <p>Naam</p>
+                <p>Jaar</p>
+                <p>Plaats</p>
+            </div>
+        </div>
+    </div>
+    <div class="grid-item">6</div>
+
+</div>
+</body>
 </html>
