c717974a36d0b51161a7e0626daca3d34eba659f
Pedro <pmecmail@gmail.com>
2023-11-01 13:06:16 +0100

    editting eader

--- ProfileApp/home.php
+++ ProfileApp/home.php
@@ -1,17 +1,20 @@
 <?php
-echo "JAMAN";
-    ?>
+include 'navigation/nav.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
 </head>
+
 <body>
-    
+
 
     <!-- Your content here -->
 
 </body>
+
 </html>
--- ProfileApp/navigation/nav.php
+++ ProfileApp/navigation/nav.php
@@ -2,19 +2,18 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>About Me</title>
-<header class="header">
-  <div id="header">
-    <nav class="navigation">
-      <img src="./img/logo-no-background.png" class="header-logo" alt="Logo">
+<header>
+  <div class="logo">
+
+    <img src="./img/logo-no-background.png" class="header-logo" alt="Logo">
+    <nav>
       <center>
         <ul>
           <li><a href="home.php"><button>Home</button></a></li>
           <li><a href="login.php"><button>login</button></a></li>
           <li><a href="register.php"><button>register</button></a></li>
           <li><a href="myprojects.php"><button>My Work</button></a></li>
-          <li><a href="profiel.php"><button>profiel</button></a>
-            <a href="settings.php"><button>About Me</button></a>
-          </li>
+          <li><a href="profiel.php"><button>Profile</button></a></li>
           <li><a href="settings.php"><button>Settings</button></li>
         </ul>
       </center>
