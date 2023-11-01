04910ddb1fe50a360d4f11c88be8f4662dfa7357
Pedro <pmecmail@gmail.com>
2023-11-01 14:56:47 +0100

    created header and fixed issues

--- ProfileApp/navigation/nav.php
+++ ProfileApp/navigation/nav.php
@@ -4,15 +4,13 @@
 <title>About Me</title>
 <header>
   <div class="logo">
-    <img src="./img/logo-no-background.png" class="header-logo" alt="Logo">
+    <a href="home.php"><img src="./img/icon.png" class="header-logo" alt="Logo"></a>
     </div>
     <nav>
         <ul>
           <li><a href="home.php"><button>Home</button></a></li>
-          <li><a href="login.php"><button>login</button></a></li>
-          <li><a href="register.php"><button>register</button></a></li>
-          <li><a href="myprojects.php"><button>My Work</button></a></li>
-          <li><a href="profiel.php"><button>Profile</button></a></li>
+          <li><a href="projects.php"><button>My Work</button></a></li>
+          <li><a href="profile.php"><button>Profile</button></a></li>
           <li><a href="settings.php"><button>Settings</button></li>
         </ul>
     </nav>
--- ProfileApp/settings.php
+++ ProfileApp/settings.php
@@ -9,15 +9,20 @@
   require 'navigation/nav.php';
   ?>
   <br>
+  <a href="home.php">
   <button class="back-btn">
     <span>Go back</span>
   </button>
+  </a>
 </head>
 
 <body>
+  <div class="white-box">
+  <div class="settings-container"> 
   <a class="profileSettings" href="profile.php">Profile Settings</a>
   <br>
   <a class="loginSecurity" href="loginSecurity.php">Login & Security</a>
+</div>
 </body>
 
 </html>
