2ae6422a8847096f9c7f0093c6ac3f42a8328e49
Pedro <pmecmail@gmail.com>
2024-01-26 14:57:58 +0100

    finished header dropdown

--- ProfileApp/navigation/nav.php
+++ ProfileApp/navigation/nav.php
@@ -10,20 +10,27 @@
       <!--  The href attribute specifies the URL that the link should navigate to when clicked. -->
         <ul>
         <img src="./img/logo-no-background.png" class="header-logo" alt="Logo">
-          <li><a href="login.php"><button>Login</button></a></li>
-          <li><a href="index.php"><button>Register</button></a></li>
           <li><a href="home.php"><button>Home</button></a></li>
           <li><a href="education.php"><button>Education</button></a></li>
           <li><a href="hobby.php"><button>My hobby</button></a></li>
           <li><a href="work.php"><button>My Work</button></a></li>
-          <li><a href="Logout.php"><button>Log out</button></a>
-         
-
-
+          
           </li>
         </ul>
       </center>
     </nav>
+  </div>
+  <div class="dropdown">
+    <!-- Add the dropdown button -->
+    <button class="dropbtn">Menu</button>
+
+    <!-- Create the dropdown content -->
+    <div class="dropdown-content">
+      <!-- Add your list items inside the dropdown content -->
+      <a href="login.php">Login</a>
+      <a href="index.php">Register</a>
+      <a href="Logout.php">Log out</a>
+    </div>
   </div>
 </header>
 
